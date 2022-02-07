6a48b92ddab9f132434f57113220b9a94a9e9780
Alexhan <46094938+hanhyeonkyu@users.noreply.github.com>
2022-02-07 10:41:54 +0900

    feat: first commmit

---
+++ src/List.svelte
@@ -0,0 +1,63 @@
+<script>
+  import Summary from "./Summary.svelte";
+
+  const PAGE_SIZE = 20;
+
+  export let page;
+
+  let items;
+  let offset;
+
+  $: fetch(`https://node-hnapi.herokuapp.com/news?page=${page}`)
+    .then((r) => r.json())
+    .then((data) => {
+      items = data;
+      offset = PAGE_SIZE * (page - 1);
+      window.scrollTo(0, 0);
+    });
+</script>
+
+{#if items}
+  {#each items as item, i}
+    <Summary {item} {i} {offset} />
+  {/each}
+  <div class="pageNums">
+    {#if page - 1 > 1 ? page - 1 : 1 !== page}
+      <a href="#/top/{page - 1 > 1 ? page - 1 : 1}"
+        >{page - 1 > 1 ? page - 1 : 1}</a
+      >
+    {/if}
+    <div class="curPageNum">{page}</div>
+    <a href="#/top/{page + 1}">{page + 1}</a>
+  </div>
+{:else}
+  <p class="loading">loading...</p>
+{/if}
+
+<style>
+  a {
+    padding: 1em;
+    display: block;
+  }
+  .curPageNum {
+    text-align: center;
+    padding: 1em;
+  }
+  .pageNums {
+    display: flex;
+    align-items: center;
+  }
+  .loading {
+    opacity: 0;
+    animation: 0.4s 0.8s forwards fade-in;
+  }
+
+  @keyframes fade-in {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+</style>
